779c03e39509624fa6f60c1046cd79ffa65901d7
KrystianBachta <krystian@fhi.pl>
2026-08-05 16:32:23 +0200

    Update MyApp.swift

--- Sources/MyApp.swift
+++ Sources/MyApp.swift
@@ -1,7 +1,19 @@
 import SwiftUI
+import GoogleMobileAds
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        // Uruchomienie SDK od Google AdMob
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+        return true
+    }
+}
 
 @main
 struct MyApp: App {
+    // Podpięcie AppDelegate do cyklu życia aplikacji SwiftUI
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
