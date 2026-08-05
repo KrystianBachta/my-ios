faee6c6cf9906430f1cafbcf117d99b6a6fe36ed
KrystianBachta <krystian@fhi.pl>
2026-08-05 13:38:09 +0200

    Update MyApp.swift

--- Sources/MyApp.swift
+++ Sources/MyApp.swift
@@ -8,30 +8,3 @@ struct MyApp: App {
         }
     }
 }
-
-struct ContentView: View {
-    @State private var trackingNumber = ""
-    
-    var body: some View {
-        NavigationView {
-            List {
-                Section(header: Text("Śledzenie przesyłek")) {
-                    TextField("Wprowadź numer listu przewozowego", text: $trackingNumber)
-                    Button(action: {
-                        // Akcja wyszukiwania
-                    }) {
-                        Text("Szukaj")
-                            .frame(maxWidth: .infinity, alignment: .center)
-                    }
-                }
-                
-                Section(header: Text("Aktywne zlecenia logistyczne")) {
-                    Text("Brak aktywnych tras")
-                        .foregroundColor(.gray)
-                        .italic()
-                }
-            }
-            .navigationTitle("Moje Zlecenia")
-        }
-    }
-}
