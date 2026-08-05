f4c75f82dacb8f38e2600f5204d6ee19150c19ef
KrystianBachta <krystian@fhi.pl>
2026-08-05 16:12:22 +0200

    Update ContentView.swift

--- Sources/ContentView.swift
+++ Sources/ContentView.swift
@@ -7,13 +7,13 @@ struct WebView: UIViewRepresentable {
     func makeUIView(context: Context) -> WKWebView {
         let webView = WKWebView(frame: .zero)
         
-        // Rozwiązanie problemu nr 2: Oszukanie blokady logowania Google
         webView.customUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1"
-        
-        // Dodatkowe parametry dla poprawnego przewijania
         webView.scrollView.bounces = false
         webView.isOpaque = false
         webView.backgroundColor = UIColor.clear
+        
+        // Ta linijka usuwa biały pasek na dole
+        webView.scrollView.contentInsetAdjustmentBehavior = .never
         
         return webView
     }
@@ -26,7 +26,6 @@ struct WebView: UIViewRepresentable {
 
 struct ContentView: View {
     var body: some View {
-        // Rozwiązanie problemu nr 1: Wymuszenie pełnej szerokości i wysokości
         WebView(url: URL(string: "https://my.fhi.pl")!)
             .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
             .ignoresSafeArea()
