943be7b32bdf60dee8a3b366842178d37abb0d10
KrystianBachta <krystian@fhi.pl>
2026-08-05 17:01:28 +0200

    Update MyApp.swift

--- Sources/MyApp.swift
+++ Sources/MyApp.swift
@@ -1,22 +1,27 @@
 import SwiftUI
 import GoogleMobileAds
+import AppTrackingTransparency
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        // Uruchomienie SDK od Google AdMob
-        GADMobileAds.sharedInstance().start(completionHandler: nil)
         return true
     }
 }
 
 @main
 struct MyApp: App {
-    // Podpięcie AppDelegate do cyklu życia aplikacji SwiftUI
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onReceive(NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification)) { _ in
+                    // Prośba o zgodę na śledzenie (ATT) wymagana przez Apple
+                    ATTrackingManager.requestTrackingAuthorization { status in
+                        // Inicjalizacja reklam po odpowiedzi użytkownika
+                        GADMobileAds.sharedInstance().start(completionHandler: nil)
+                    }
+                }
         }
     }
 }
